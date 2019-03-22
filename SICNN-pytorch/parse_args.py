3ff12e137e4cc659b042ef9524714a7a823f75c0
顾煜贤 <2017011421@secoder.net>
2019-03-22 16:33:16 +0800

    optimizer code

--- SICNN-pytorch/parse_args.py
+++ SICNN-pytorch/parse_args.py
@@ -4,4 +4,7 @@ parser = argparse.ArgumentParser(description="SICNN-pytorch Implementation")
 
 parser.add_argument("--train", action="store_true", help="train the model")
 
+parser.add_argument("--epochs", action="store", type=int, help="epochs to train")
+parser.add_argument("--bs", action="store", type=int, help="batch size")
+
 args = parser.parse_args()
--- SICNN-pytorch/train.py
+++ SICNN-pytorch/train.py
@@ -6,20 +6,57 @@ import torch.nn.functional as F
 from parse_args import parser
 from parse_args import args
 
+from dataset import ImageDataset
 import model as M
+import net_sphere
 
-def train(args):
+EuclidianLoss = nn.MSELoss()
+AngleLoss = net_sphere.AngleLoss()
+
+def train(sicnn):
+    cnnh = sicnn.cnnh
+    cnnr = sicnn.cnnr
+
+    ds = ImageDataset(args, args.bs)
+
+    for i in range(args.epochs):
+
+def trainBatch(sicnn, inputHR, inputLR, label, optimizerCNNR, optimizerCNNH):
+    """
+    inputHR: HR image, Variable
+    inputLR: LR image, Variable
+    label: face label for HR image, Variable
+    """
+    SR = sicnn.cnnh(inputLR)
+
+    #CNNR
+    optimizerCNNR.zero_grad()
+    catHRSR = torch.cat([inputHR, SR], 0)
+    catLabel = torch.cat([label, label], 0)
+    SI = sicnn.cnnr(catHRSR)
+    LFR = AngleLoss(SI, catLabel)
+    LFR.backward()
+    optimizerCNNR.step()
+
+    #CNNH
+    optimizerCNNH.zero_grad()
+    LSR = EuclideanLoss(SR, inputHR)
+    LSR.backward()
+    optimizerCNNH.step()
+
+
+
+
+def eval():
     pass
 
-def eval(args):
-    pass
-
-def main(args):
+def main():
     if args.train:
         sicnn = M.SICNN(args)
         cnnh = sicnn.cnnh
         cnnr = sicnn.cnnr
-        
+
+
 
 
 if __name__ == "__main__":
